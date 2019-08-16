76c2cd0bb96285e7f1398b036360dc24d821f5b1
Nanx <dynamicheard88@gmail.com>
2019-08-16 16:27:41 +0700

    add dashboard

---
+++ application/modules/Dashboard/controllers/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends MX_Controller {
+
+    public function __construct(){
+        parent::__construct();
+    }
+    public function index(){ 
+            $this->load->view('dashboard');
+    }
+
+   
+}
---
+++ application/modules/Dashboard/views/dashboard.php
@@ -0,0 +1,85 @@
+<html>
+
+<head>
+    <title>Olive Customer Service</title>
+</head>
+<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" />
+
+<body>
+    <div class="background">
+        <div class="bg-isi">
+            <div class="h-transparan row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="header-left">
+                        <img src="<?php echo base_url(); ?>assets/images/OLIVE LOGO WHITE.png" height="60px;" width="auto;" />
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <div class="header-right">
+                        <div class="center">
+                            <a href="#">
+                        <img src="<?php echo base_url(); ?>assets/images/logout white.png" height="30px;" width="30px;" />
+                        <p class="tx-icon">Logout</p>
+                    </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="isi">
+                <div class="col-md-4">
+                    <div class="tx-isi">Selamat datang <br>
+                        <b>DIAN SASTRO WARDOYO <br> (Customer Service)</b><br> Selamat bekerja
+                    </div>
+                </div>
+                <div class="col-md-4 p-15">
+                    <div class="isi-center">
+                        <img src="<?php echo base_url(); ?>assets/images/OLIVE LOGO WHITE.png" height="250px;" width="auto;" />
+                        <div class="div-btn">
+                            <a href="<?=base_url('register')?>">
+                                <button class="btn-isi text-white">PELAYANAN</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                </div>
+            </div>
+
+            <div class="footer">
+                <div class="col-md-4 fot-bar">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>assets/images/doctor.png" height="auto" width="70px" />
+                    </div>
+                    <div class="col-md-9" style="text-align:left;">
+                        <div class="fot_title_bar">SETTING LAYANAN</div>
+                        <div class="fot_subtitle_bar">Pengaturan perawatan, paket, & layanan periksa</div>
+                    </div>
+                </div>
+                <div class="col-md-4 fot-bar-pilih">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>assets/images/discount white.png" height="auto" width="70px" />
+                    </div>
+                    <div class="col-md-9" style="text-align:left;">
+                        <div class="fot_title_bar">SETTING PROMO</div>
+                        <div class="fot_subtitle_bar">Pengaturan promo dan diskon pada setiap layanan yang ditawarkan</div>
+                    </div>
+                </div>
+                <div class="col-md-4 fot-bar">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>assets/images/box.png" height="auto" width="70px" />
+                    </div>
+                    <div class="col-md-9" style="text-align:left;">
+                        <div class="fot_title_bar">SETTING MERCHANT</div>
+                        <div class="fot_subtitle_bar">Pengaturan merchant yang bekerjasama dengan perusahaan</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
